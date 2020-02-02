b5dae3369a69d2a05bab2bceeb22376d3edeaf94
Ajay Basnyat <ajaybasnyat@gmail.com>
2020-02-01 20:11:44 -0600

    added link to student list

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from flask import Flask, render_template
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='mrbartucz.com',
+                             user='fx9785rk',
+                             password='Pernambucano.wolf',
+                             db='fx9785rk_university',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Select all Students
+        sql = "SELECT StudentID, FirstName, LastName from Student"
+        
+        # execute the SQL command
+        cursor.execute(sql)
+        
+        studentList = ""
+        # get the results
+        for student in cursor:
+            
+        
+finally:
+    connection.close()
+
 
 app = Flask(__name__)
 
@@ -9,6 +35,10 @@ def index():
 @app.route('/cakes')
 def cakes():
     return 'Yummy cakes!'
+
+@app.route('/students')
+def students():
+    return  studentList
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
